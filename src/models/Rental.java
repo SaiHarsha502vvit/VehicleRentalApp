1ddd783d922ff3c327b7cfb2f26877d11d39391e
anusha29052003 <159566112+anusha29052003@users.noreply.github.com>
2026-03-10 10:00:34 +0530

    added rental class

---
+++ src/models/Rental.java
@@ -0,0 +1,50 @@
+import java.time.LocalDateTime;
+
+class Rental
+{
+    private String rentalId;
+    private User user;
+    private Vehicle vehicle;
+    private LocalDateTime startTime;
+    private LocalDateTime actualReturnTime;
+    private RentalStatus status;
+
+   
+    Rental(String rentalId, User user, Vehicle vehicle)
+    {
+        this.rentalId = rentalId;
+        this.user = user;
+        this.vehicle = vehicle;
+
+       
+        this.startTime = LocalDateTime.now();
+
+      
+        this.status = RentalStatus.ACTIVE;
+    }
+
+    
+    LocalDateTime getStartTime()
+    {
+        return startTime;
+    }
+
+    
+    Vehicle getVehicle()
+    {
+        return vehicle;
+    }
+
+    
+    RentalStatus getStatus()
+    {
+        return status;
+    }
+
+    
+    void closeRental()
+    {
+        actualReturnTime = LocalDateTime.now();
+        status = RentalStatus.CLOSED;
+    }
+}
