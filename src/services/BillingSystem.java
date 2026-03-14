373f35d72bca95f54c86d7590218f8b9ac2a00cd
Amulya Reddy <93787176+AmulyaReddylyagala@users.noreply.github.com>
2026-03-14 09:20:43 +0530

    changed var name

--- src/services/BillingSystem.java
+++ src/services/BillingSystem.java
@@ -22,14 +22,14 @@ public class BillingSystem {
         double total = minutes * pricePer24Hrs;
 
         long hrs = minutes / 60;
-        long minutes = minutes % 60;
+        long Myminutes = minutes % 60;
 
         System.out.println("\n  ============== BILL ==============");
         System.out.println("  Vehicle     : " + rental.getVehicle().getModelName()
                 + "  [" + rental.getVehicle().getVehicleType() + "]");
         System.out.println("  Start Time  : " + start.format(FMT));
         System.out.println("  Return Time : " + end.format(FMT));
-        System.out.printf("  Duration    : %d hr  %d min%n", hrs, minutes);
+        System.out.printf("  Duration    : %d hr  %d min%n", hrs, Myminutes);
         System.out.printf("  Rate        : Rs.%d / 24 hrs%n", (int) pricePer24Hrs);
         System.out.printf("  Total Bill  : Rs.%.2f%n", total);
         System.out.println("  ==================================");
